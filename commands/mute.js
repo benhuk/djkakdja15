2ba4afc8e6ca3b6c54d290da0d99933ca7ba3472
benhuk <felipebenhuk85@gmail.com>
2018-05-29 14:23:38 -0300

    Update mute.js

--- commands/mute.js
+++ commands/mute.js
@@ -5,14 +5,14 @@ module.exports.run = async (bot, message, args) => {
 
     if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Não!");
     let tomute = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-    if (!tomute) return message.reply("Use: +mute <tempo> <@user>");
-    if (tomute.hasPermission("MANAGE_MESSAGES")) return message.reply("Esse user não pode ser mutado!");
-    let muterole = message.guild.roles.find(`name`, "Mutado");
+    if (!tomute) return message.reply("Use: +mute <tempo> <@user>.");
+    if (tomute.hasPermission("MANAGE_MESSAGES")) return message.reply("O usuário que você está tentando silenciar é igual ou maior que você.");
+    let muterole = message.guild.roles.find(`name`, "Muted");
 
     if (!muterole) {
         try {
             muterole = await message.guild.createRole({
-                name: "Mutado",
+                name: "Muted",
                 color: "#000000",
                 permissions: []
             })
@@ -28,14 +28,14 @@ module.exports.run = async (bot, message, args) => {
     }
 
     let mutetime = args[1];
-    if (!mutetime) return message.reply("Use: +mute <tempo> <@user>");
+    if (!mutetime) return message.reply("Use: +mute <tempo> <@user>.");
 
     await (tomute.addRole(muterole.id));
     message.reply(`<@${tomute.id}> foi mutado por: ${ms(ms(mutetime))}`);
 
     setTimeout(function() {
         tomute.removeRole(muterole.id);
-        message.channel.send(`<@${tomute.id}> Foi desmutado!`);
+        message.channel.send(`<@${tomute.id}> foi desmutado!`);
     }, ms(mutetime));
 
 }
