f13a020513eee60f49f88a02227b2d8240f3c7be
benhuk <felipebenhuk85@gmail.com>
2018-05-29 14:02:49 -0300

    Update mute.js

--- commands/mute.js
+++ commands/mute.js
@@ -1,18 +1,41 @@
-  var Discord = require('discord.js')
- var ms = require("ms")
-exports.run = (client,message,args)=>{
- 
-let member= message.mentions.members.first();
-if (!member) return message.reply("Mencione um membro!");
-let muteRole = message.guild.roles.find("name","Mutado");
-if (!muteRole) return message.reply ("crie um cargo de mute!")
-let params = message.content.split(" ").slice(1);
-let time = params[1];
-if (!time) return message.reply("Determine o temp de mute");
+const Discord = require("discord.js");
+const ms = require("ms");
 
-member.addRole(muteRole.id)
- message.channel.send(`membro foi mutado com sucesso!, ${member.user.tag}`);
-            };
-  module.exports.help = {
-  name: "mute"
+module.exports.run = async (bot, message, args) => {
+
+    if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Não!");
+    let tomute = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    if (!tomute) return message.reply("Use: +mute <tempo> <@user>");
+    if (tomute.hasPermission("MANAGE_MESSAGES")) return message.reply("Esse user não pode ser mutado!");
+    let muterole = message.guild.roles.find(`name`, "Mutado");
+
+    if (!muterole) {
+        try {
+            muterole = await message.guild.createRole({
+                name: "Mutado",
+                color: "#000000",
+                permissions: []
+            })
+            message.guild.channels.forEach(async (channel, id) => {
+                await channel.overwritePermissions(muterole, {
+                    SEND_MESSAGES: false,
+                    ADD_REACTIONS: false
+                });
+            });
+        } catch (e) {
+            console.log(e.stack);
+        }
+    }
+
+    let mutetime = args[1];
+    if (!mutetime) return message.reply("Use: +mute <tempo> <@user>");
+
+    await (tomute.addRole(muterole.id));
+    message.reply(`<@${tomute.id}> foi mutado por: ${ms(ms(mutetime))}`);
+
+    setTimeout(function() {
+        tomute.removeRole(muterole.id);
+        message.channel.send(`<@${tomute.id}> Foi desmutado!`);
+    }, ms(mutetime));
+
 }
